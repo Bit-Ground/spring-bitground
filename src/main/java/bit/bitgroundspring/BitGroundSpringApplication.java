df0abead66f13f1dc7f0c99c9ccda3f47cb22d1a
jongwon-jeon <jw6963@naver.com>
2025-05-19 13:42:20 +0900

    docker test: add jenkinsfile

--- src/main/java/bit/bitgroundspring/BitGroundSpringApplication.java
+++ src/main/java/bit/bitgroundspring/BitGroundSpringApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-//@SpringBootApplication
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
+//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 @EntityScan
 @EnableJpaRepositories
 public class BitGroundSpringApplication {
