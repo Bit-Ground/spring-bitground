cf7145765af90fe15c06350e4115d0be5926a933
leejaeho-17 <woghdl99@naver.com>
2025-05-26 18:22:03 +0900

    리셋

--- src/main/java/bit/bitgroundspring/dto/BoardDto.java
+++ src/main/java/bit/bitgroundspring/dto/BoardDto.java
@@ -1,4 +1,27 @@
 package bit.bitgroundspring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardDto {
+    private Integer id;
+    private Integer userId;      // User 엔티티 대신 ID만
+    private String name;     // 응답용: 작성자 이름
+    private String title;
+    private String content;
+    private String filePath;
+    private String fileName;
+    private int likes;
+    private int reports;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
+    private boolean isDeleted;
+    private String category;
 }
