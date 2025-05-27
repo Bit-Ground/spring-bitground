b4882c79e79c671b9c7a1d0997fabf018d0bf605
jihoon <tjwlgns2004@naver.com>
2025-05-27 17:42:44 +0900

    주석처리 내용 수정

--- src/main/java/bit/bitgroundspring/repository/RankingRepository.java
+++ src/main/java/bit/bitgroundspring/repository/RankingRepository.java
@@ -8,4 +8,8 @@ import java.util.List;
 public interface RankingRepository extends JpaRepository<UserRanking, Integer > {
     // 시즌 ID로 해당 시즌의 유저 랭킹 리스트 조회 (총 자산 내림차순 정렬)
     List<UserRanking> findBySeason_IdOrderByTotalValueDesc(int seasonId);
+
+    // 특정 시즌 기준으로 랭킹 전체를 총자산 내림차순으로 정렬해서 조회
+//    List<UserRanking> findBySeasonOrderByTotalValueDesc(Season season);
+
 }
