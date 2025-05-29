6620e3e5808dbdf78320974867f0a3773a758797
haneeoh <honey9473@gmail.com>
2025-05-29 14:26:00 +0900

    등락률 그래프 백데이터

---
+++ src/main/java/bit/bitgroundspring/controller/GbIndexController.java
@@ -0,0 +1,23 @@
+package bit.bitgroundspring.controller;
+
+import bit.bitgroundspring.service.GbIndexService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/gbindex")
+public class GbIndexController {
+    private final GbIndexService gbIndexService;
+
+    public GbIndexController(GbIndexService gbIndexService) {
+        this.gbIndexService = gbIndexService;
+    }
+
+    @GetMapping
+    public Map<String, Map<String, Double>> getGbIndex() {
+        return gbIndexService.calculateGbIndices();
+    }
+}
