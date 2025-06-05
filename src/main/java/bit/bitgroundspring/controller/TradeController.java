0c97516525ef441fa0b864e3076ded1592b34509
jongwon-jeon <jw6963@naver.com>
2025-06-05 19:22:02 +0900

    trade history 웹소켓(진행중) - UNKNOWN 값 불러와짐 - 실시간으로 되지 않는 현상

--- src/main/java/bit/bitgroundspring/controller/TradeController.java
+++ src/main/java/bit/bitgroundspring/controller/TradeController.java
@@ -1,10 +1,43 @@
 package bit.bitgroundspring.controller;
 
+import bit.bitgroundspring.dto.TradeDto;
+import bit.bitgroundspring.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/trade")
 public class TradeController {
+    @Autowired
+    OrderService orderService;
+
+    //    @GetMapping("/history")
+//    public ResponseEntity<List<TradeDto>> getTradeHistory(
+//            @RequestParam("symbol") String symbol,
+//            @RequestParam(value = "since", required = false) String sinceStr
+//    ) {
+//        if (sinceStr == null) {
+//            // since 파라미터가 없으면 최신 100개 전체 조회
+//            List<TradeDto> history = orderService.getRecentTrades(symbol);
+//            return ResponseEntity.ok(history);
+//        }
+//        // since 파라미터가 있으면 그 시간 이후의 변경분만 조회
+//        LocalDateTime since = LocalDateTime.parse(sinceStr);
+//        List<TradeDto> newTrades = orderService.getNewTradesSince(symbol, since);
+//        return ResponseEntity.ok(newTrades);
+//    }
+    @GetMapping("/history")
+    public ResponseEntity<List<TradeDto>> getTradeHistory(
+            @RequestParam("symbol") String symbol
+    ) {
+        List<TradeDto> history = orderService.getRecentTrades(symbol);
+        return ResponseEntity.ok(history);
+    }
 }
--- src/main/java/bit/bitgroundspring/service/OrderService.java
+++ src/main/java/bit/bitgroundspring/service/OrderService.java
@@ -1,10 +1,14 @@
 package bit.bitgroundspring.service;
 
+import bit.bitgroundspring.dto.OrderDto;
+import bit.bitgroundspring.dto.TradeDto;
 import bit.bitgroundspring.dto.projection.OrderProjection;
+import bit.bitgroundspring.entity.Status;
 import bit.bitgroundspring.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -15,5 +19,14 @@ public class OrderService {
 
     public List<OrderProjection> getOrdersBySeason(Integer seasonId, Integer userId) {
         return orderRepository.findBySeasonIdAndUserId(seasonId, userId);
+    }
+
+    public List<TradeDto> getRecentTrades(String symbol) {
+        return orderRepository.findTop30ByCoinSymbolAndStatus(symbol, Status.COMPLETED);
+    }
+
+    // 이후 변경분만
+    public List<TradeDto> getNewTradesSince(String symbol, LocalDateTime since) {
+        return orderRepository.findNewTradesByCoinSymbolAndStatusSince(symbol, Status.COMPLETED, since);
     }
 }
