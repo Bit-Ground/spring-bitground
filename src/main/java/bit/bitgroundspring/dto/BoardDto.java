8fae127270bbf9a75d362cacc32f128a3af70319
leejaeho-17 <woghdl99@naver.com>
2025-06-11 10:48:30 +0900

    0611 오전 게시글 삭제, 사진표시작업

--- src/main/java/bit/bitgroundspring/dto/BoardDto.java
+++ src/main/java/bit/bitgroundspring/dto/BoardDto.java
@@ -24,4 +24,27 @@ public class BoardDto {
     private String category;     // Enum.name() 형식으로 전달
     private int views;
     private Long commentCount;
+    private boolean hasImage;
+
+    public BoardDto(Integer id, Integer userId, String name, String title, String content,
+                    int tier, int likes, int dislikes, boolean isDeleted,
+                    LocalDateTime createdAt, LocalDateTime updatedAt, String category,
+                    int views, Long commentCount) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
+        this.title = title;
+        this.content = content;
+        this.tier = tier;
+        this.likes = likes;
+        this.dislikes = dislikes;
+        this.isDeleted = isDeleted;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.category = category;
+        this.views = views;
+        this.commentCount = commentCount;
+        this.hasImage = false; // 기본값 (나중에 setHasImage로 따로 설정 가능)
+    }
 }
+
