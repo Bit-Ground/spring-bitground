9e55637055244dd55bfd0f2afba56d7ffc546c9e
leejaeho-17 <woghdl99@naver.com>
2025-06-12 19:18:48 +0900

    0612 오후 공지사항 페이지 작업

--- src/main/java/bit/bitgroundspring/controller/NoticeController.java
+++ src/main/java/bit/bitgroundspring/controller/NoticeController.java
@@ -1,4 +1,30 @@
 package bit.bitgroundspring.controller;
 
+import bit.bitgroundspring.dto.NoticeRequestDto;
+import bit.bitgroundspring.dto.NoticeResponseDto;
+import bit.bitgroundspring.entity.Notice;
+import bit.bitgroundspring.service.NoticeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/notices")
+@RequiredArgsConstructor
 public class NoticeController {
+    private final NoticeService noticeService;
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody NoticeRequestDto dto) {
+        Notice saved = noticeService.createNotice(dto);
+        return ResponseEntity.ok(saved);
+    }
+
+    @GetMapping
+    public Page<NoticeResponseDto> getNotices(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return noticeService.getPagedNoticeDtos(page, size);
+    }
 }
