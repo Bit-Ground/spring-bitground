8bcfe0606836fd35903da9bb3dee447d60fd77ba
Macro4760 <seho4760@gmail.com>
2025-06-12 18:50:06 +0900

    coincontroller 수정

---
+++ src/main/java/bit/bitgroundspring/dto/AiInsightSymbolDto.java
@@ -0,0 +1,17 @@
+// src/main/java/bit/bitgroundspring/dto/AiInsightSymbolDto.java
+
+package bit.bitgroundspring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AiInsightSymbolDto {
+    private String symbol;
+    private String koreanName;
+}
