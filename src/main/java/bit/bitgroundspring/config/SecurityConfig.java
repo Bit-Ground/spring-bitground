6f4132b11a7bce99375c99338981500bc2ed5101
haneeoh <honey9473@gmail.com>
2025-06-18 12:57:42 +0900

    마켓인덱스 api 수정

--- src/main/java/bit/bitgroundspring/config/SecurityConfig.java
+++ src/main/java/bit/bitgroundspring/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
                         }))
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
                         .requestMatchers("/api/auth/refresh", "/auth/refresh", "/oauth2/**", "/error", "/favicon.ico", "/test","/mypage/**",
-                                "/api/news/**", "/api/coin/**", "/api/public/**", "/public/**", "/auth/logout", "/api/market-index/**",
+                                "/api/news/**", "/api/coin/**", "/api/public/**", "/public/**", "/auth/logout", "/market-index/**",
                                 "/api/docs/**", "/actuator/*", "/ws/**", "/seasons/update").permitAll()
                         .anyRequest().authenticated()
                 )
--- src/main/java/bit/bitgroundspring/controller/MarketIndexController.java
+++ src/main/java/bit/bitgroundspring/controller/MarketIndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/market-index")
+@RequestMapping("/market-index")
 public class MarketIndexController {
     private final MarketIndexService marketIndexService;
 
